2fe86593f1f0433cf8c4eed642c3114c72e4ba8f
David Schwerd <dmsch656@gmail.com>
2022-02-23 15:21:54 -0700

    switch statement

--- index.js
+++ index.js
@@ -39,7 +39,32 @@ function givePrompts(){
                 },
             ]
         }
-    ])
+    ]).then(res => {
+        let choice = res.choice;
+        switch(choice) {
+            case "VIEW_DEPARTMENTS":
+                viewDepartments();
+                break;
+            case "VIEW_ROLES":
+                viewRoles();
+                break;
+            case "VIEW_EMPLOYEES":
+                viewEmployees();
+                break;
+            case "ADD_DEPARTMENT":
+                addDepartment();
+                break;
+            case "ADD_ROLE":
+                addRole();
+                break;
+            case "ADD_EMPLOYEE":
+                addEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+                break;
+        }
+    })
 }
 
 function init() {
