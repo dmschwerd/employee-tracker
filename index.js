2f23195e83e16c016cb0ada2065f3652a67a17de
David Schwerd <dmsch656@gmail.com>
2022-02-23 15:12:31 -0700

    prompt

--- index.js
+++ index.js
@@ -3,7 +3,43 @@ const db = require("./db");
 require("console.table");
 
 function givePrompts(){
-
+    prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "Pick an option from the list",
+            choices: [
+                {
+                    name: "View all departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
+                {
+                    name: "View all roles",
+                     value: "VIEW_ROLES"
+                },
+                {
+                    name: "View all employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "Add department",
+                    value: "ADD_DEPARTMENT"
+                },
+                {
+                    name: "Add role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "Add employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+            ]
+        }
+    ])
 }
 
 function init() {
